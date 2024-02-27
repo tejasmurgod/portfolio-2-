1e28f2a9055ae63fe796b0724c16a3cd65e33afe
tejas murgod <tejasmurgod07@example.com>
2024-02-27 15:43:07 +0530

    git

---
+++ script.js
@@ -0,0 +1,81 @@
+// *****************toggle icon navbar************************
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+};
+//******************* */ scroll section active items
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            });
+        };
+
+    });
+
+    // *********************sticky nav bar*******************
+
+    let header = document.querySelector('header');
+
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+   // ***************** remove toggle icon & navbar when click navbar link(scroll)************************
+
+   menuIcon.classList.remove('bx-x');
+   navbar.classList.remove('active');
+
+};
+
+
+    // *********************sscroll reveal*******************
+
+    ScrollReveal({
+        reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200,
+    });
+
+    ScrollReveal().reveal('.about-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.about-img', { origin: 'left' });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Data Analyst", 
+"Backend Developer", "Fit-ness Freak"],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop: true
+})
